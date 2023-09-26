2c714fb26e6f38ba10d563a6bf279b3d95f3b623
kian <kian.edj@gmail.com>
2023-09-26 16:23:53 +0330

    otp Auth by Email

---
+++ accounts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
